b04780e4f976b09f52f2bf4fd56f89e37aaff2c3
Pouya Khoshnavazi <pouyanavazi@gmail.com>
2025-05-20 22:19:50 -0700

    Figured out structure of scss and react components in project architecture. Will use this to design basic frontend.

---
+++ src/components/App.tsx
@@ -0,0 +1,50 @@
+import * as Query from '../adzuna/query'
+import * as Parse from '../adzuna/parse'
+import * as Auth from '../supabase/auth'
+import React from 'react'
+
+export default function App() {
+    return (
+        <div>
+            <header>
+                <h1>
+                    By Pouya Khoshnavazi
+                </h1>
+            </header>
+        </div>
+    )
+}
+
+// export default function App() {
+
+//   return (
+//     <div className="df flex-col vh-100">
+//       <header>
+//         <h1 className="mt0 mb0">Create New App</h1>
+//         <div>By Pouya Khoshnavazi</div>
+//       </header>
+
+//       <section>
+//         <div>
+//           Your application starts in the{' '}
+//           <code>
+//             src/<span className="b white">entry.jsx</span>
+//           </code>{' '}
+//           file.
+//         </div>
+
+//         <div>
+//           The component you're looking here at can be found in{' '}
+//           <code>
+//             src/components/<span className="b white">App.jsx</span>
+//           </code>
+//         </div>
+
+//         <div>
+//           Now go! Save the world with <span className="gold">JavaScript</span>!
+//         </div>
+
+//       </section>
+//     </div>
+//   )
+// }
