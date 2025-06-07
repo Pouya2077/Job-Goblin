d63973faa97a679884751892553c8b48181f159c
Pouya Khoshnavazi <pouyanavazi@gmail.com>
2025-06-07 00:28:39 -0700

    Adding react router capabilities.

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -3,42 +3,24 @@ import * as Parse from '../adzuna/parse'
 import * as Auth from '../supabase/auth'
 import React, {useState, useEffect} from 'react'
 import {Job} from "../types"
-
+import { Sign } from 'crypto'
 
 export default function App() {
-    const [result, newResult] = useState<Job | null>(null);
-    let job: Job
-
-    useEffect(() => {
-        Query.simpleQuery(10, "software developer")
-        .then(response => {
-            job = response.data.results[0];
-            newResult(job);
-        })
-        .catch(error => {
-            console.log("Fetched data is corrupted: ", error.data.error);
-        })
-        
-    }, []);
-    
-    if (!result) {
-        return (
-            <div>
-                <header>
-                    <h1>Loading...</h1>
-                </header>
-            </div>
-        )
-    }
 
     return (
         <div>
             <header>
-                <h1>
-                    By Pouya Khoshnavazi
-                    <p>{result.title}</p>
-                </h1>
+                <h1>Welcome to Job Goblin!</h1>
             </header>
+            
+            <div>
+                <button>
+                    Login
+                </button>
+                <button>
+                    SignUp
+                </button>
+            </div>
         </div>
     )
 
---
+++ src/components/test.tsx
@@ -0,0 +1,43 @@
+import * as Query from '../adzuna/query'
+import * as Parse from '../adzuna/parse'
+import * as Auth from '../supabase/auth'
+import React, {useState, useEffect} from 'react'
+import {Job} from "../types"
+
+export default function Test() {
+    const [result, setResult] = useState<Job | null>(null);
+    let job: Job
+
+    useEffect(() => {
+        Query.simpleQuery(10, "software developer")
+        .then(response => {
+            job = response.data.results[0];
+            setResult(job);
+        })
+        .catch(error => {
+            console.log("Fetched data is corrupted: ", error.data.error);
+        })
+        
+    }, []);
+    
+    if (!result) {
+        return (
+            <div>
+                <header>
+                    <h1>Loading...</h1>
+                </header>
+            </div>
+        )
+    }
+
+    return (
+        <div>
+            <header>
+                <h1>
+                    By Pouya Khoshnavazi
+                    <p>{result.title}</p>
+                </h1>
+            </header>
+        </div>
+    )
+}
