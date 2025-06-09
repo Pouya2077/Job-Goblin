9a283ecceb8c727fb7a149aad148c918e24b8790
Pouya Khoshnavazi <pouyanavazi@gmail.com>
2025-06-08 22:48:46 -0700

    Added basic page naviation with react-router-dom and useNavigate React hook.

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -9,8 +9,8 @@ export default function App() {
     return (
         <Routes>
             <Route path="/" element={<Home></Home>} />
-            <Route path="/Login" element={<Login></Login>} />
-            <Route path="/SignUp" element={<SignUp></SignUp>} />
+            <Route path="/login" element={<Login></Login>} />
+            <Route path="/signup" element={<SignUp></SignUp>} />
         </Routes>
     )
 
--- src/components/Home.tsx
+++ src/components/Home.tsx
@@ -1,12 +1,19 @@
 import React from "react"
+import {useNavigate} from "react-router-dom"
 
 export default function Home() {
+    const navigate = useNavigate();
+
     return (
         <div>
             <header>
                 <h1>
                     Welcome to Job Goblin!
                 </h1>
+                <div>
+                    <button onClick={() => {navigate("/login")}}>Login</button>
+                    <button onClick={() => {navigate("/signup")}}>SignUp</button>
+                </div>
             </header>
         </div>
     )
