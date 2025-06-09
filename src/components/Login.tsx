a23ec5e8b593ec965938867a54eeaaaee3eccdbf
Pouya Khoshnavazi <pouyanavazi@gmail.com>
2025-06-08 23:29:22 -0700

    Start of basic user authentication frontend.

--- src/components/Login.tsx
+++ src/components/Login.tsx
@@ -1,13 +1,44 @@
 import React from "react"
+import {useNavigate} from "react-router-dom"
+import {useEffect, useState} from "react"
+import * as Auth from "./../supabase/auth"
 
-export default function Login() {
+function LoginPanel() {
+    let navigate = useNavigate();
+    let email: string;
+    let password: string;
+    const [inputValue, setInputValue] = useState("");
+
+    const inputChanged = (event: any) => {
+        if (event.target.value != "") {
+            setInputValue(event.target.value);
+        }
+    }
+
+    useEffect(() => {
+        if (inputValue != "") {
+            navigate("/signup");
+        }
+    }, [inputValue]);
+
     return (
         <div>
-            <header>
-                <button>
-                    Login!
-                </button>
-            </header>
+            <h1>Username</h1>
+            <input type="text" value={inputValue} onChange={inputChanged} />
+
+            <h2>Password</h2>
+            <input type="password" />
+        </div>
+    )
+}
+
+export default function Login() {
+
+
+    return (
+        <div>
+            <h1>Login!</h1>
+            <LoginPanel />
         </div>
     )
 }
