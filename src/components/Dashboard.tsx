cd8d1163ffd5a819ab06e916850a5646da1a1119
Pouya Khoshnavazi <pouyanavazi@gmail.com>
2025-06-12 08:32:23 -0700

    Added blueprint design of the Dashboard. Will implement its functionality so user can switch between companies.

--- src/components/Dashboard.tsx
+++ src/components/Dashboard.tsx
@@ -1,10 +1,49 @@
 import React from "react"
+import {useState} from "react"
 
+/**
+ * @description will prompt user to select company, then
+ *              selected company will be loaded by CompanyPanel
+ */
+function CompanySelector() {
+        return (
+            <div>
+                <title>Companies:</title>
+            </div>
+        )
+
+}
+
+/**
+ * @description will load in selected company 
+ *              onto dashboard
+ */
+function CompanyPanel({company}: {company: string}) {
+    switch (company) {
+        default: return (
+            <div>
+                <h1>No Company Selected</h1>
+            </div>
+        )
+
+    }
+
+}
+
+/**
+ * @description will simply load in visual and functional
+ *              components, the hub where job query 
+ *              parameters are selected by the user
+ */
 export default function Dashboard() {
+    const [companyName, setCompanyName] = useState("");
+
     return (
         <div>
             <header>
-                <h1>This is the Dashboard!</h1>
+                <h1>Welcome </h1>
+                <CompanySelector/>
+                <CompanyPanel company={companyName}/>
             </header>
         </div>
     )
