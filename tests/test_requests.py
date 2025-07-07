8e8cd2386753d9f17ed9744d9d337e64ea812c8f
Pouya Khoshnavazi <pouyanavazi@gmail.com>
2025-07-06 21:44:10 -0700

    Basic project environment setup finished. Did basic testing to see how API works with pyhon requests library.

---
+++ tests/test_requests.py
@@ -0,0 +1,18 @@
+import requests
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+URL = "http://api.adzuna.com/v1/api/jobs/ca/search/1?"
+APP_ID = os.getenv("ADZUNA_ID")
+APP_KEY = os.getenv("ADZUNA_KEY")
+
+response = requests.get(URL, {
+    "app_id": APP_ID,
+    "app_key": APP_KEY, 
+    "results_per_page": 2, 
+    "what": "software developer"
+})
+
+print(response.status_code)
+print(response.json())
