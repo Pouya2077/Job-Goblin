0508431c3cfa57125bd374e39d3a72566bd98f58
Pouya Khoshnavazi <pouyanavazi@gmail.com>
2025-07-09 19:10:52 -0700

    Updated project structure, will figure out setup.py and configure it for easy package importing throughout project.

--- tests/test_emailing.py
+++ tests/test_emailing.py
@@ -1,10 +1,15 @@
 """ Experimenting with basic email capabilities """
 
-from src.fetching.fetcher import Fetcher
+#TODO finish this testing after finishing setup.py for easy package importing
 
-BASE_URL = "http://api.adzuna.com/v1/api/jobs/ca/search/1?"
-WHAT = "software developer"
-RESULTS_PER_PAGE = 1
+# from src.fetching.fetcher import Fetcher
 
-Adzuna = Fetcher(BASE_URL, "ADZUNA", {"what": WHAT, "results_per_page": RESULTS_PER_PAGE})
+# BASE_URL = "http://api.adzuna.com/v1/api/jobs/ca/search/1?"
+# WHAT = "software developer"
+# RESULTS_PER_PAGE = 1
+
+# Adzuna = Fetcher(BASE_URL, "ADZUNA", {"what": WHAT, "results_per_page": RESULTS_PER_PAGE})
+
+# response = Adzuna.canada_jobs()
+# print(response)
 
