03a17b5343e035e8ba963ca51ba90dc6fa0c2239
Pouya Khoshnavazi <pouyanavazi@gmail.com>
2025-08-01 23:27:10 -0700

    Minor changes.

--- src/event_logging/log.py
+++ src/event_logging/log.py
@@ -3,7 +3,7 @@ import logging
 
 LOG = logging.getLogger("job_goblin")
 LOG.setLevel(logging.DEBUG)
-LOG.propogate = False       #avoid root debugger
+LOG.propogate = False       #avoid root logger
 
 file_handler = logging.FileHandler("app.log", encoding="utf-8", mode="a")
 file_handler.setLevel(logging.DEBUG)
@@ -13,6 +13,6 @@ formatter = logging.Formatter(
     style="{",
     datefmt="%Y-%m-%d %H:%M",
 )
-file_handler.setFormatter(formatter)
 
+file_handler.setFormatter(formatter)
 LOG.addHandler(file_handler)
